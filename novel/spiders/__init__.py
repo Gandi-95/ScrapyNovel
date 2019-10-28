c5a6dff1b1a0c75e32c5294b6c478e9052fe3f25
Gandi-95 <609531039@qq.com>
2019-10-28 23:47:49 +0800

    save

--- novel/spiders/__init__.py
+++ novel/spiders/__init__.py
@@ -2,3 +2,20 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+
+if __name__ == '__main__':
+    num_enum = {
+        '零': 0, '一': 1, '二': 2, '三': 3, '四': 4, '五': 5, '六': 6, '七': 7, '八': 8, '九': 9, '两': 2, '千': '', '百': '', '十': '',
+        '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9
+    }
+    name = '阳间篇 第1022章 雷震子'
+    listnum = list(name)
+    print( num_enum.values())
+    num = []
+    for i in listnum:
+        if i in num_enum.keys():
+            num.append(num_enum[i])
+        if i in num_enum.values():
+            num.append(i)
+
+    print(num)
--- novel/spiders/novelSpider.py
+++ novel/spiders/novelSpider.py
@@ -12,46 +12,18 @@ class NovelspiderSpider(BaseSpider):
     allowed_domains = ['www.lingdianshuwu.com']
     start_urls = ['https://www.lingdianshuwu.com/','https://www.lingdianshuwu.com/search.asp?searchlist=%s&SearchClass=1']
 
-    def start_requests(self):
-        # self.input_novelName = input("请输入小说名：")
-        self.input_novelName = "圣墟"
-        url = self.start_urls[1] % urllib.parse.quote(self.input_novelName.encode('gbk'))
-        # url = 'https://www.lingdianshuwu.com/search.asp?searchlist=%CA%A5&SearchClass=1'
-        yield scrapy.Request(url=url, callback=self.parse)
+    def startUrl(self,input_novelName):
+        return self.start_urls[1] % urllib.parse.quote(self.input_novelName.encode('gbk'))
 
-    def parse(self, response):
-        novel = self.getSeachNovel(response)
-        print(novel)
-        self.input_novelName = novel[0]
-        return scrapy.http.Request(url=novel[1], callback=self.parseCatalog, dont_filter=True)
-
-
-
-    def selectNovel(self, novel_list):
-        for index, novel in enumerate(novel_list):
-            print(str(index + 1) + '：' + novel[0])
-
-        return 1;
-        index = input("请选择：")
-        try:
-            index = int(index)
-            if (index <= 0 and index > len):
-                index = self.selectNovel('请输入正确下载的序号:', len)
-        except Exception as e:
-            index = self.selectNovel('请输入正确下载的序号:', len)
-        return index
-
-    def getSeachNovel(self, response):
+    def parseSeachNovel(self, response):
         novel_list = []
         for sel in response.xpath('//span[@class="M"]'):
             title = sel.xpath('a/text()').extract()
             url = sel.xpath('a/@href').extract()
             if len(title) > 0 and len(url) > 0:
                 novel_list.append((title[0], self.start_urls[0] + url[0].replace('books', 'contents')))
+        return novel_list
 
-        index = self.selectNovel(novel_list)
-
-        return novel_list[index-1]
 
 
     def parseCatalog(self, response):
